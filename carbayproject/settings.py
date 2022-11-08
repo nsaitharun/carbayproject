91464f58e8305ecbdba39259e3ffea974ec0df87
sai tharun <nsaitharun2408@gmail.com>
2022-11-08 12:34:36 +0530

    tested git status

--- carbayproject/settings.py
+++ carbayproject/settings.py
@@ -77,13 +77,13 @@ WSGI_APPLICATION = 'carbayproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR , 'db.sqlite3'),
     }
 }
 
 
 # Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
+# https://docs.djanghoproject.com/en/4.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR , 'static')
+STATICFILES_ =[
+os.path.join(BASE_DIR,'carbayproject/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
